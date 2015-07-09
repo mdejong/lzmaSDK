70d8da8b4a2d1960c5ec91c53540754e9260fcf9
Mo DeJong <modejong@Mos-MacBook-Air.local>
2015-07-09 15:41:27 -0700

    add target for a dynamic Framework build for iOS 8 and newer build systems

---
+++ lzmaSDK/lzmaSDK.h
@@ -0,0 +1,19 @@
+//
+//  lzmaSDK.h
+//  lzmaSDK
+//
+//  Created by Mo DeJong on 7/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for lzmaSDK.
+FOUNDATION_EXPORT double lzmaSDKVersionNumber;
+
+//! Project version string for lzmaSDK.
+FOUNDATION_EXPORT const unsigned char lzmaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lzmaSDK/PublicHeader.h>
+
+#import "LZMAExtractor.h"
